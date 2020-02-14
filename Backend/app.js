60dc141e7d28d4956211f1e6bbd3827b366e5d4d
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2020-02-13 22:33:03 -0500

    Configuracion de colores para logs

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const config = require('./app/config');
+const colors = require('colors');
+
+colors.setTheme({
+    info: 'green',
+    help: 'magenta',
+    warn: 'yellow',
+    success: 'cyan',
+    error: 'red'
+});
 
 app.listen(config.PORT, () => {
-    console.log('=========================================================');
-    console.log(`**** Servidor inicializado en puerto ${config.PORT}`);
-    console.log('=========================================================');
+    console.log('========================================================='.info);
+    console.log(`**** Servidor inicializado en puerto ${config.PORT}`.success);
+    console.log('========================================================='.info);
 });
